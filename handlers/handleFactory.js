04651424ec6fb0e9b1705ff0d338eddadff4ae31
Izzattillakhodza Makhmudkhodzaev <khodzapro@gmail.com>
2022-11-28 07:56:05 -0500

    seller role created

--- handlers/handleFactory.js
+++ handlers/handleFactory.js
@@ -9,7 +9,7 @@ exports.createOne = (Model) => catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: {
-      data: doc,
+      doc,
     },
   });
 });
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,7 +5,7 @@ const productController = require('../controllers/productController');
 
 const router = express.Router();
 
-router.route('/').get(productController.getAllProducts).post(authController.protect, authController.restrictTo('admin'), productController.addProduct);
+router.route('/').get(productController.getAllProducts).post(authController.protect, authController.restrictTo('admin','seller'), productController.addProduct);
 
-router.route('/:id').get(productController.getProduct).patch(authController.protect, authController.restrictTo('admin'), productController.updateProduct).delete(authController.protect, authController.restrictTo('admin'), productController.deleteProduct);
+router.route('/:id').get(productController.getProduct).patch(authController.protect, authController.restrictTo('admin','seller'), productController.updateProduct).delete(authController.protect, authController.restrictTo('admin','seller'), productController.deleteProduct);
 module.exports = router;
