8981e00c7fd45ee3e1f163f0cb7622502ad29abd
Izzattillakhodza <96073464+khodza@users.noreply.github.com>
2022-11-29 09:14:16 +0500

    Update reviewRoute.js
    acces user for deleting review

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -8,6 +8,6 @@ const router =express.Router();
 
 router.route('/').get(authController.protect,authController.restrictTo('admin'),reviewController.getAllReviews).post(authController.protect,authController.restrictTo('user'),reviewController.addReview)
 
-router.route('/:id').get(reviewController.getReview).patch(authController.protect,authController.restrictTo('user'),reviewController.updateReview).delete(authController.protect,authController.restrictTo('admin'),reviewController.deleteReview)
+router.route('/:id').get(reviewController.getReview).patch(authController.protect,authController.restrictTo('user'),reviewController.updateReview).delete(authController.protect,authController.restrictTo('admin','user'),reviewController.deleteReview)
 
 module.exports =router;
